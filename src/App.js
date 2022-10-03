bc032eaaaec009dc88dc1eda0cf1f629890335e2
amitthapliyal <103377739+amitthapliyal@users.noreply.github.com>
2022-10-03 08:19:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,52 +27,7 @@ return [...prevEvents,event]
 
   return (
     <div className='App'>
-      <Title title="import React, { useState } from 'react'
-import './App.css'
-import Modal from './component/Modal'
-import Eventlist from './component/Eventlist'
-import Title from './component/Title'
-import EventListForm from './component/EventListForm'
-
-export default function App() {
-  const [modal, showModal] = useState(false)
-  const [ShowEvent, setShowEvent] = useState(true)
-  const [Event, newEvent] = useState([])
-  
-  const handleDelete = (n) => {
-    newEvent((prevEvent) => {
-      return prevEvent.filter((event) => {
-        return n !== event.id;
-      })
-    })
-  }
-  const addEvent = (event) => {
-    newEvent((prevEvents)=>{
-return [...prevEvents,event]
-    })
-    showModal(false)
-  }
-  
-
-  return (
-    <div className='App'>
       <Title title=" Add/Remove Events " changeColor={false} />
-      {!ShowEvent &&
-        <button onClick={() => setShowEvent(true)}>Show Button</button>}
-      {ShowEvent &&
-        <button onClick={() => setShowEvent(false)}>Hide Button</button>}
-
-      {ShowEvent &&
-        <Eventlist Event={Event} handleDelete={handleDelete} />
-      }
-      {modal && <Modal >
-        <EventListForm addEvent={addEvent}/>
-      </Modal>}
-      <button onClick={() => showModal(true)}>Add Event</button>
-    </div>
-  )
-}
-" changeColor={false} />
       {!ShowEvent &&
         <button onClick={() => setShowEvent(true)}>Show Button</button>}
       {ShowEvent &&
